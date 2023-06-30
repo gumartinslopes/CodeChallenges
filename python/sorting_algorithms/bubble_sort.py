be7f1882c9b115d54055ae852afe63a54a6b1609
Gustavo Martins <gumartinslopes@gmail.com>
2023-06-30 09:42:50 -0300

    update bubble sort

--- python/sorting_algorithms/bubble_sort.py
+++ python/sorting_algorithms/bubble_sort.py
@@ -1,10 +1,15 @@
+import numpy as np
+
 # Complexity = On^2 in all three cases
-
-
 def bubble_sort(values):
-    pass
-
-
-numbers = [4,9,2,1,7,8]
-
-bubble_sort(numbers)
+    result = values.copy()
+    for i in range(len(result)):
+        for j in range(len(result) - i-1):
+            if result[j] > result[j+1]:
+                aux = result[j]
+                result[j] = result[j+1]
+                result[j+1] = aux
+    return result
+random_values = np.random.randint(0, 10, 10)
+print(random_values)
+print(bubble_sort(random_values))
---
+++ python/sorting_algorithms/readme.md
@@ -0,0 +1,4 @@
+# Sorting Algorithms
+## Bubble Sort O(n^2)
+The simplest sorting algorithm. Its complexity is O(n^2) in all three cases as it will always go
+through all n elements n times.
