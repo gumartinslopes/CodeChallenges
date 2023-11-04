084df9d9f4a3e6edd996e0416c1bf45cb84e6470
Gustavo Martins <gumartinslopes@gmail.com>
2023-11-04 11:27:04 -0300

    update two sum python

---
+++ python/sorting_algorithms/Insertion_sort.py
@@ -0,0 +1,6 @@
+insertion_sort(values):
+	pass
+	
+random_values = np.random.randint(0, 10, 10)
+print(random_values)
+print(bubble_sort(random_values))
--- python/two_sum.py
+++ python/two_sum.py
@@ -5,24 +5,29 @@ Given an array of integers nums and an integer target, return indices of the two
 up to target. You may assume that each input would have exactly one solution, and you may not use the same 
 element twice. You can return the answer in any order.
 '''
-# O(n) solution
-def twoSum(nums, target):
-    prevMap = {}
+# # O(n) solution
+# def twoSum(nums, target):
+#     prevMap = {}
+#     for i, el in enumerate(nums):
+#         difference = target - el
+#         if difference in prevMap:
+#             return [prevMap[difference], i]
+#         prevMap[el] = i
+
+def two_sum(nums, target):
+    pos_map = {}
     for i, el in enumerate(nums):
-        difference = target - el
-        if difference in prevMap:
-            return [prevMap[difference], i]
-        prevMap[el] = i
-        
+        diff = target - el
+        if diff in pos_map:
+             return [pos_map[diff], i]
+        pos_map[el] = i
+
 # O(n^2) solution
-def twoSum2(nums, target):
-        result = []
+def two_sum2(nums, target):
         for i in range(len(nums)):
             for j in range(i+ 1, len(nums)):
                 if nums[i] + nums[j] == target:
-                    result.append(i)
-                    result.append(j)
-        return result
+                    return [i, j]
 
 # should return [0, 1]
-twoSum([2,7,11,15], 9)
+assert two_sum([2,7,11,15], 9) == [0, 1]
